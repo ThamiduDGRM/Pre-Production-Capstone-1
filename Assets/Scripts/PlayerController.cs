ed2851243c02025c47df034b97578911c56045cf
ThamiduDGRM <thamidu21@gmail.com>
2026-03-21 01:58:28 +1100

    PlayerWalk&IdleAnimationImplementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private int speed = 5;    
-
+    [SerializeField] private int speed = 5;
+    [SerializeField] private Animator animator;
+    
+    [SerializeField] private SpriteRenderer playerSprite;
     private PlayerControls playerControls;
     private Rigidbody rb;
     private Vector3 movement;
     private Vector3 lastDirection = Vector3.forward;
-
+    private const string IS_MOVING_PARAM = "IsMoving";
     private void Awake()
     {
         playerControls = new PlayerControls();
@@ -33,14 +35,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // FIXED: Use .Move instead of .Movement
         Vector2 moveInput = playerControls.Player.Move.ReadValue<Vector2>();
 
         float x = moveInput.x;
         float z = moveInput.y;
 
         movement = new Vector3(x, 0, z).normalized;
-        
+        animator.SetBool(IS_MOVING_PARAM, movement!=Vector3.zero);
+
+        if (x!=0 && x< 0)
+
+        {
+            playerSprite.flipX = true;
+        }
+
+        if (x!=0 && x> 0)
+
+        {
+            playerSprite.flipX = false;
+        }
+
+
     }
 
     private void FixedUpdate()
@@ -48,4 +63,5 @@ public class PlayerController : MonoBehaviour
         rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
     }
 }
+
 
