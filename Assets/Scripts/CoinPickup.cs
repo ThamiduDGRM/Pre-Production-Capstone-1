a86f0b6d7ad074e880f3f2cb69681e1d043ee1d3
ThamiduDGRM <thamidu21@gmail.com>
2026-03-25 23:41:09 +1100

    CoinCollider

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public int value = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Add to player currency here
+            Debug.Log("Picked up coin!");
+
+            Destroy(gameObject);
+        }
+    }
+}
+
