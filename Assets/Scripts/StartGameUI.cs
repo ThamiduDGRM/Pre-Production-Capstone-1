60b5d95daf8c820fd518499aed4199f7f15dc6f7
ThamiduDGRM <thamidu21@gmail.com>
2026-03-27 00:52:21 +1100

    StartGameButtonUi

---
+++ Assets/Scripts/StartGameUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StartGameUI : MonoBehaviour
+{
+    public GameObject startPanel;
+
+    private void Start()
+    {
+        // Pause the game at the beginning
+        Time.timeScale = 0f;
+        startPanel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        // Unpause the game
+        Time.timeScale = 1f;
+        startPanel.SetActive(false);
+    }
+}
+
