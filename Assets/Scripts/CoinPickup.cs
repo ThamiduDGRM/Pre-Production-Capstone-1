b2ccf4c39fa9b072daf876418e7d651eba32a531
ThamiduDGRM <thamidu21@gmail.com>
2026-04-25 22:38:49 +1000

    furtherdevelopment

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -10,7 +10,7 @@ public class CoinPickup : MonoBehaviour
         {
             // Add to player currency here
             Debug.Log("Picked up coin!");
-            CurrencyManager.Instance.AddCoins(5);
+            CurrencyManager.Instance.AddCoins(1);
             Destroy(gameObject);
         }
     }
