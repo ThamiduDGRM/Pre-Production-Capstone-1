b511ef357453855d44784c937bd19f4477c9e085
ThamiduDGRM <thamidu21@gmail.com>
2026-06-29 20:10:34 +1000

    InstructionsSampleUiAdded

--- Assets/Scripts/StartGameUI.cs
+++ Assets/Scripts/StartGameUI.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class StartGameUI : MonoBehaviour
 {
     public GameObject startPanel;
+    public GameObject instrustionsPanel;
 
     private void Start()
     {
         // Pause the game at the beginning
         Time.timeScale = 0f;
         startPanel.SetActive(true);
+        instrustionsPanel.SetActive(true);
     }
 
     public void StartGame()
@@ -16,6 +18,7 @@ public class StartGameUI : MonoBehaviour
         // Unpause the game
         Time.timeScale = 1f;
         startPanel.SetActive(false);
+        instrustionsPanel.SetActive(false);
     }
 }
 
